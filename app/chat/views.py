da474556bd77c88493e1cdbd333f517a7489e352
Akbarjon <abdurasuloff2004@gmail.com>
2023-09-27 18:02:42 +0500

    3-dars

--- app/chat/views.py
+++ app/chat/views.py
@@ -15,11 +15,11 @@ def chat(request, username):
     user1 = request.user
     user2 = User.objects.get(username=username)
 
-    chatgroup = ChatGroup.objects.filter(users=user1).filter(users=user2)
+    chatgroup = ChatGroup.objects.filter(users=user1).filter(users=user2).first()
 
     if not chatgroup:
         chatgroup = ChatGroup.objects.create(name=f"chat_between_{user1.username}_and_{user2.username}")
         chatgroup.users.add(user1, user2)
 
-    context = {"users":users,  "user":user2}
+    context = {"users":users,  "user":user2, "chatgroup":chatgroup }
     return render(request, "chat.html", context)
